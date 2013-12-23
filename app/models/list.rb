369e544bf70a7534070b1da1040a384e1fd7d0d6
nathanallen <thenathanator@gmail.com>
2013-12-23 14:52:22 -0800

    ensure uniqueness of url

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ActiveRecord::Base
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :subscriptions
   has_many :users, through: :subscriptions
+  validates_uniqueness_of :url
 end
