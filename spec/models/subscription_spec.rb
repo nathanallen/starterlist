650fdc970f0d7e16d1507f29d8fda10ee07ff7d5
nathanallen <thenathanator@gmail.com>
2013-12-23 14:54:28 -0800

    test that list owner is subscribed to list

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe Subscription do
-  it { should belong_to(:user) }
-  it { should belong_to(:list) }
+  context "database associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:list) }
+  end
+
+  context "on creating a new list" do
+    it "ensures list owner is a subscriber" do
+      new_user = User.create(email: "test@test.com")
+      new_list = List.create(url: "test", owner: new_user)
+      new_subscription = Subscription.where(list_id: new_list.id)
+      expect(new_subscription.last.user_id).to eq(new_user.id)
+    end
+  end
 end
