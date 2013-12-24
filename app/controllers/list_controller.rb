dda328f7a19b76a72feb557acb3f0645e11dc3a9
nathanallen <thenathanator@gmail.com>
2013-12-24 10:11:57 -0800

    spiked list routes returning json

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,2 +1,30 @@
 class ListController < ApplicationController
+  respond_to :json
+  
+  #GET /list/new.json
+  def new
+    render json: { list: 'success' }.to_json
+  end
+
+  #POST /list/
+  def create
+    render json: { status: 'posted successfully' }.to_json
+  end
+
+  #GET /list/:custom_url
+  def show
+    list = params['custom_url']
+    render json: { name: list }.to_json
+  end
+
+  #UPDATE /list/:custom_url
+  def update
+    render json: { status: 'updated successfully' }.to_json
+  end
+
+  # #DELETE /list/:custom_url
+  # def destroy
+
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Starterlist::Application.routes.draw do
   root to: 'home#index'
-
-  #get '/:custom_url'
+  #resources :list, only: [:create]
+  
+  get '/list/new.:format' => 'list#new', constraints: {:format => /json/}
+  post '/list/create.:format' => 'list#create', constraints: {:format => /json/}
+  get 'list/:custom_url.:format' => 'list#show', constraints: {:format => /json/}
+  patch '/list/:custom_url.:format' => 'list#update', constraints: {:format => /json/}
+  get '/:custom_url' => 'home#index'
 
   # get '/quizzes.:format', to: 'quizzes#index', as: :quizzes, constraints: {:format => /json/}
   # get '/quizzes/:quiz_id/questions/next.:format', to: 'questions#next', as: :next_question, constraints: {:format => /json/}
