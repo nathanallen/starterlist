5117cd156af52cb8d6ccf2e3cdeafe6141c25375
nathanallen <thenathanator@gmail.com>
2013-12-26 20:52:41 -0800

    list controller with api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Starterlist::Application.routes.draw do
   root to: 'home#index'
+
+  #API
+  get '/list/new.:format' => 'list#new', constraints: {:format => /json/}
+  post '/list/create.:format' => 'list#create', constraints: {:format => /json/}
+  get 'list/:custom_url.:format' => 'list#show', constraints: {:format => /json/}
+  patch '/list/:custom_url.:format' => 'list#update', constraints: {:format => /json/}
+  
+  #Wildcard Route for Custom URLs
+  get '/:custom_url' => 'home#index'
 end
