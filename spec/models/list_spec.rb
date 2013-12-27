9f36ddd749271696f20f72bf7643c6033f507efc
nathanallen <thenathanator@gmail.com>
2013-12-26 22:02:58 -0800

    test that custom pathname cannot be model name 'list'

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,4 +5,5 @@ describe List do
   it { should have_many(:users).through(:subscriptions) }
   it { should validate_uniqueness_of(:url) }
   it { should_not allow_value('/?=illegal', '#symbol', 'punctuat!on', 'space ' ).for(:url) }
+  it { should_not allow_value('list').for(:url) }
 end
