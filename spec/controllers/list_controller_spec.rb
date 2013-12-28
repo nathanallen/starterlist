9f617bbecd7f7c3307050a5f201f719e292e2db0
nathanallen <thenathanator@gmail.com>
2013-12-28 03:30:07 -0800

    controller tests for list creation & subscription via api

---
+++ spec/controllers/list_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe ListController do
+
+  valid_input = {requested_name: Time.now.to_i.to_s, owner_email: "owner@email.com"}
+  invalid_input = valid_input #list name now exists in database
+
+  context "Creating a new list" do
+    describe "with an available name" do
+      it "returns status:success" do
+        post 'create', valid_input
+        response_json.should eq({status: "success"})
+      end
+    end
+
+    describe "with a taken name" do
+      it "returns an error" do
+        post 'create', invalid_input
+        response_json.should eq({status: "failed", error_message: "Name not available"})
+      end
+    end
+  end
+
+  context "Subscribing to a list" do
+    describe "that exists" do
+      it "returns status:success" do
+        post 'subscribe', valid_input
+        response_json.should eq({status: "success"})
+      end
+    end
+
+    describe "that does not exist" do
+      it "returns an error" do
+        post 'subscribe', list_name: "!doesnotexist", email: "unique@email.com"
+        response_json.should eq({status: "failed", error_message: "List does not exist"})
+      end
+    end
+  end
+
+end
+
+def response_json
+  JSON.parse(response.body)
+end
