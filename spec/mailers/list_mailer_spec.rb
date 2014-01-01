4934b1f38be48f4e980b656a256aa3b3f9cbd416
nathanallen <thenathanator@gmail.com>
2013-12-31 19:33:26 -0800

    mailer unit tests

--- spec/mailers/list_mailer_spec.rb
+++ spec/mailers/list_mailer_spec.rb
@@ -1,46 +1,61 @@
 require "spec_helper"
 
 describe ListMailer do
+  let(:owner) {User.create!(email: Faker::Internet.email)}
+  let(:list) { List.create!(url: Faker::Internet.domain_word, owner: owner)}
+
   describe "creation_confirmation_email" do
-    let(:mail) { ListMailer.creation_confirmation_email }
+    let(:mail) { ListMailer.creation_confirmation_email(list) }
 
     it "renders the headers" do
-      mail.subject.should eq("Creation confirmation email")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Get started with your new list!")
+      mail.to.should eq([owner.email])
+      mailed_from_list_email?
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      correct_link?
     end
   end
 
   describe "subscription_confirmation_email" do
-    let(:mail) { ListMailer.subscription_confirmation_email }
+    let(:user) { User.create!(email: Faker::Internet.email) }
+    let(:subscription) { list.subscribe(user) }
+    let(:mail) { ListMailer.subscription_confirmation_email(subscription) }
 
     it "renders the headers" do
-      mail.subject.should eq("Subscription confirmation email")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Spread the word!")
+      mail.to.should eq([user.email])
+      mailed_from_list_email?
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      correct_link?
     end
   end
 
   describe "email_blast" do
-    let(:mail) { ListMailer.email_blast }
+    let(:mail) { ListMailer.email_blast(list) }
 
     it "renders the headers" do
-      mail.subject.should eq("Email blast")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Your email list is ready")
+      mail.to.should eq([owner.email, user.email])
+      mailed_from_list_email?
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      list.emails.each do |email|
+        mail.body.encoded.should match(email)
+      end
     end
+  end
+
+  def mailed_from_list_email?
+    mail.from.should eq(["#{list.url}@starterlist.com"])
+  end
+
+  def correct_link?
+    mail.body.encoded.should match("http://starterlist.com/#{list.url}")
   end
 
 end
